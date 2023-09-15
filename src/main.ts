a7e1211f7337db7d951f3bad3275182149e7b9f9
rds <dartorandroid@gmail.com>
2023-09-15 07:56:09 -0300

    cors test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('NestJS With JWT')

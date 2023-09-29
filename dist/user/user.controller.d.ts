206c72ce3ec91f617dcb2148d3d2ce4a1b85850b
rds <dartorandroid@gmail.com>
2023-09-29 18:52:36 -0300

    test

--- dist/user/user.controller.d.ts
+++ dist/user/user.controller.d.ts
@@ -1,7 +1,7 @@
-import { UsersService } from "./user.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { User } from "./entities/user.entity";
+import { UsersService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 export declare class UsersController {
     private readonly userService;
     constructor(userService: UsersService);

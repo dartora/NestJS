76827832c6cc06aa23b325f8c167517e83ee9498
rds <dartorandroid@gmail.com>
2023-09-30 20:55:27 -0300

    testing vercel

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
 
 @Module({
   controllers: [CommentController],
-  providers: [CommentService]
+  providers: [CommentService],
+  imports: [TypeOrmModule.forFeature([Comment])],
+
 })
-export class CommentModule {}
+export class CommentModule { }

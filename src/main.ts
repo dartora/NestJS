ea46bab7ceab82ecf83a1b073b937d4bbfb93e16
rds <dartorandroid@gmail.com>
2023-09-30 22:00:08 -0300

    teste1

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerOptions } from 'swagger-ui-express';
-import * as swaggerUI from 'swagger-ui-dist';
+import * as swaggerUI from 'swagger-ui-express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
@@ -16,10 +16,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.use("/", swaggerUI.SwaggerUIBundle, swaggerUI.SwaggerUIStandalonePreset(document));
+  app.use("/", swaggerUI.serve, swaggerUI.setup(document));
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap().catch((err) => console.error(err));
-

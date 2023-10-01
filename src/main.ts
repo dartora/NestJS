917ed91626944fcb696e4483e22d6e2da973f9a8
rds <dartorandroid@gmail.com>
2023-09-30 22:35:29 -0300

    testano

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as swaggerUI from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,8 +15,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.use("/", swaggerUI.serve, swaggerUI.setup(document));
-
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }

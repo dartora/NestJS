77a817e7e91cb9a0efab5caaa5339bb6c90489cf
rds <dartorandroid@gmail.com>
2023-09-30 22:34:06 -0300

    testando

--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-// core
-import { resolve } from 'path';
-import { writeFileSync, createWriteStream } from 'fs';
-import { get } from 'http';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as swaggerUI from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
-  const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+  const config = new DocumentBuilder()
+    .setTitle('NestJS With JWT')
+    .setDescription('The DARTORA TI API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('studies')
     .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/swagger', app, document);
 
-  await app.listen(process.env.PORT || 3000);
-  app.enableCors()
-  // get the swagger json file (if app is running in development mode)
-  if (process.env.NODE_ENV === 'development') {
-    let serverUrl = 'https://nest-js-wine.vercel.app/'
-    // write swagger ui files
-    get(
-      `${serverUrl}/swagger/swagger-ui-bundle.js`, function
-      (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-      console.log(
-        `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
-      );
-    }).on('error', function (err) {
-      console.log('Error loading swagger-ui-bundle.js:', err);
-    });
+  const document = SwaggerModule.createDocument(app, config);
+  app.use("/", swaggerUI.serve, swaggerUI.setup(document));
 
-    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-      console.log(
-        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-      );
-    }).on('error', function (err) {
-      console.log('Error loading swagger-ui-init.js:', err);
-    });
-
-    get(
-      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-        );
-      }).on('error', function (err) {
-        console.log('Error loading swagger-ui-standalone-preset.js:', err);
-      });
-
-    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-      console.log(
-        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-      );
-    }).on('error', function (err) {
-      console.log('Error loading swagger-ui.css:', err);
-    });
-
-  }
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
 }
-
-bootstrap();
-console.log("im still getting on webbroser oading failed for the <script> with source “https://nest-6xd0nh75m-dartora.vercel.app/swagger/swagger-ui-bundle.js”. swagger:69:45");
-console.log("Loading failed for the <script> with source “https://nest-6xd0nh75m-dartora.vercel.app/swagger/swagger-ui-standalone-preset.js”. swagger:70:56");
-console.log("from vercel cerver");
+bootstrap().catch((err) => console.error(err));

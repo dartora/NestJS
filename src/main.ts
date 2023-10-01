01b14949d773a8901912008acd4b197f02fde5bc
rds <dartorandroid@gmail.com>
2023-09-30 23:45:23 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,39 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import swaggerUI from "swagger-ui-express";
+
+import swaggerJsDoc from 'swagger-jsdoc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Library API",
+        version: "1.0.0",
+        description: "A simple Express Library API",
+        termsOfService: "http://example.com/terms/",
+        contact: {
+          name: "API Support",
+          url: "http://www.exmaple.com/support",
+          email: "support@example.com",
+        },
+      },
+      servers: [
+        {
+          url: "https://nest-js-wine.vercel.app/",
+          description: "My API Documentation",
+        },
+      ],
+    },
+    // This is to call all the file
+    apis: ["src/**/*.js"],
+  };
+
+  const specs = swaggerJsDoc(options);
 
   const config = new DocumentBuilder()
     .setTitle('NestJS With JWT')
@@ -16,10 +45,14 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   const swaggerRoute = process.env.NODE_ENV === 'production' ? '/swagger' : '/api/swagger';
-
-  SwaggerModule.setup(swaggerRoute, app, document, { customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui.min.css' });
+  app.use(
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(specs, { customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.0/swagger-ui.min.css' })
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap().catch((err) => console.error(err));
+
 

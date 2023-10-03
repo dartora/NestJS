945a9298d11fd7e28979b692b999c62ac80a6a9d
rds <dartorandroid@gmail.com>
2023-10-02 22:23:26 -0300

    a better information about deploy on home

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,9 +8,13 @@ export class AppController {
   @Get()
   getDeployInfo(): string {
     return `
+      <h1>Deployment Information</h1>
       <p>Deployment URL: ${process.env.VERCEL_URL}</p>
       <p>Git Commit SHA: ${process.env.VERCEL_GIT_COMMIT_SHA}</p>
       <p>Environment: ${process.env.VERCEL_ENV}</p>
+      <p>Git Commit Message: ${process.env.VERCEL_GIT_COMMIT_MESSAGE}</p>
+      <p>Git Commit Author Login: ${process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN}</p>
+      <p>Git Commit Author Name: ${process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME}</p>
     `;
   }
 }
